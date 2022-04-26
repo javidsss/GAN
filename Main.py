5f2fe198155bc9aa60eacf4f1e8562f045917774
Javid <j.abderezaei@gmail.com>
2022-04-25 18:04:18 -0700

    Fixed the tensorboard part

--- Main.py
+++ Main.py
@@ -19,16 +19,15 @@ Image_Width = 28
 Image_Height = 28
 Num_ColorChannels = 1
 Image_Dim_Total = Image_Width * Image_Height * Num_ColorChannels
-batch_size = 10
-num_epoch = 10
+batch_size = 32
+num_epochs = 50
 
 class Discriminator(nn.Module):
     def __init__(self, Image_Dim_Total):
         super().__init__()
-
         self.disc = nn.Sequential(
             nn.Linear(Image_Dim_Total, 128),
-            nn.LeakyReLU(0.1),
+            nn.LeakyReLU(0.01),
             nn.Linear(128, 1),
             nn.Sigmoid()
             )
@@ -41,7 +40,7 @@ class Generator(nn.Module):
         super().__init__()
 
         self.gen = nn.Sequential(nn.Linear(Noise_Dim, 256),
-                                 nn.LeakyReLU(0.1),
+                                 nn.LeakyReLU(0.01),
                                  nn.Linear(256, Image_Dim_Total),
                                  nn.Tanh(),
         )
@@ -52,10 +51,12 @@ class Generator(nn.Module):
 disc = Discriminator(Image_Dim_Total).to(device)
 gen = Generator(Noise_Dim, Image_Dim_Total).to(device)
 transforms = transforms.Compose(
-    [transforms.ToTensor(), transforms.Resize([Image_Width, Image_Height]), transforms.Normalize((0.5), (0.5))]
+    [transforms.ToTensor(), transforms.Resize([Image_Width, Image_Height]), transforms.Normalize((0.5,), (0.5,))]
 )
 
-dis_optim = torch.optim.Adam(disc.parameters(), lr=lr)
+fixed_noise_For_Tensorboard = torch.randn((batch_size, Noise_Dim)).to(device)
+
+disc_optim = torch.optim.Adam(disc.parameters(), lr=lr)
 gen_optim = torch.optim.Adam(gen.parameters(), lr=lr)
 criterion = nn.BCELoss()
 
@@ -65,26 +66,26 @@ criterion = nn.BCELoss()
 DataLoading = datasets.MNIST(root="/Users/javidabderezaei/Downloads/TransferToServer/Explicit-GAN-Project/MNIST_Data", transform=transforms, download=True)
 IterationOfTheData = DataLoader(DataLoading, batch_size=batch_size, shuffle=True)
 
-writer_fake = SummaryWriter(f"runs/GAN/fake")
-writer_real = SummaryWriter(f"runs/GAN/real")
+writer_fake = SummaryWriter(f"/Users/javidabderezaei/Downloads/TransferToServer/Explicit-GAN-Project/MNIST_Data/Tensorboard/runs/Fake")
+writer_real = SummaryWriter(f"/Users/javidabderezaei/Downloads/TransferToServer/Explicit-GAN-Project/MNIST_Data/Tensorboard/runs/Real")
 step = 0
 
-for epochs in range(num_epoch):
+for epoch in range(num_epochs):
     for Batch_Index, (Real_Image,_) in enumerate(IterationOfTheData):
         Real_Image = Real_Image.view(-1, Image_Dim_Total).to(device)
-        Noise_Rand = torch.rand(batch_size, Noise_Dim).to(device)
+        Noise_Rand = torch.randn(batch_size, Noise_Dim).to(device)
 
         gen_Fake_Image = gen(Noise_Rand)
         disc_Real_Image = disc(Real_Image)
 
         loss_disc_real = criterion(disc_Real_Image.view(-1), torch.ones_like(disc_Real_Image).view(-1))
 
-        disc_gen_Fake_Image = disc(gen_Fake_Image).detach()
+        disc_gen_Fake_Image = disc(gen_Fake_Image)
         loss_disc_fake = criterion(disc_gen_Fake_Image.view(-1), torch.zeros_like(disc_gen_Fake_Image).view(-1))
         loss_disc = (loss_disc_real + loss_disc_fake) / 2
         disc.zero_grad()
-        loss_disc.backward() #Or use retain_graph=True instead of detach in a few lines above here!
-        dis_optim.step()
+        loss_disc.backward(retain_graph=True) #Retain graph is set to True, since we are disc(gen_Fake_Image) and performing another backprop below that involves disc(gen_Fake_Image)
+        disc_optim.step()
 
         disc_gen_Fake_Image = disc(gen_Fake_Image)
         loss_gen = criterion(disc_gen_Fake_Image.view(-1), torch.ones_like(disc_gen_Fake_Image).view(-1))
@@ -92,14 +93,14 @@ for epochs in range(num_epoch):
         loss_gen.backward()
         gen_optim.step()
 
-        if Batch_Index % 100 == 0:
+        if Batch_Index % 500 == 0:
             print(f"Batch: [{Batch_Index}/{len(IterationOfTheData)}] \ "
-                  f"eEpoch: [{epochs}/{num_epoch}] \ "
+                  f"eEpoch: [{epoch}/{num_epochs}] \ "
                   f"Loss Discriminator: {loss_disc: 0.4f} & Loss Generator: {loss_gen: 0.4f}"
             )
 
             with torch.no_grad():
-                fake_Image = gen(Noise_Rand).reshape(-1, 1, Image_Height, Image_Width)
+                fake_Image = gen(fixed_noise_For_Tensorboard).reshape(-1, 1, Image_Height, Image_Width)
                 real_image_resized = Real_Image.reshape(-1, 1, Image_Height, Image_Width)
 
                 img_grid_fake = torchvision.utils.make_grid(fake_Image, normalize=True)
@@ -110,7 +111,7 @@ for epochs in range(num_epoch):
                 )
 
                 writer_real.add_image(
-                    "Fake Images", img_grid_real, global_step=step
+                    "Real Images", img_grid_real, global_step=step
                 )
 
                 step += 1
