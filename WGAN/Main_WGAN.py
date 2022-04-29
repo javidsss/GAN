5aa742b025ecdf990ad91f6aa1f06d2c9e8777f4
Javid <j.abderezaei@gmail.com>
2022-04-29 15:19:46 -0700

    Discriminator Added

--- WGAN/Main_WGAN.py
+++ WGAN/Main_WGAN.py
@@ -6,7 +6,7 @@ from torch.utils.tensorboard import SummaryWriter
 from DataLoader import FFHQ_Dataset
 from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
-from Model import Discriminator, Generator, Initialize_Weight
+from Model import critic, Generator, Initialize_Weight
 import matplotlib.pyplot as plt
 import sys
 
@@ -14,8 +14,8 @@ Model_Save = True
 Model_Load = False
 
 if Model_Load is True:
-    ModelVersion_disc = "/Model_Save_WGAN/Discriminator_Epoch0_BatchIdx60.pth"
-    ModelVersion_gen = "/Model_Save_WGAN/Generator_Epoch0_BatchIdx60.pth"
+    ModelVersion_critic = "/Model_Save_WGAN/Critic_Epochxx_BatchIdxxx.pth"
+    ModelVersion_gen = "/Model_Save_WGAN/Generator_Epochxx_BatchIdxxx.pth"
 
 FFHQdataset = False
 MNISTdataset = False
@@ -30,7 +30,7 @@ if Celebdataset == True:
     TrainDataLoc = '/Users/javidabderezaei/Downloads/TransferToServer/Explicit-GAN-Project/Celeb'
 
 ## Hyperparameters
-lr = 1e-5
+lr = 5e-5
 device = "cuda" if torch.cuda.is_available() else "cpu"
 Noise_Dim = 100
 Image_Width = 64
@@ -41,54 +41,37 @@ num_epochs = 100
 feature_d = 64
 feature_g = 64
 Num_Imgs_On_Tensorboard = 32
+Critic_Iteration = 5
+Weight_Clip = 0.01
 
+def ModelSave_func(model, optimization, loss, batch_num, epoch_num, path):
+    checkpoint = {'epoch': epoch_num, 'State_dict': model.state_dict(), 'Optimizer': optimization.state_dict(), 'loss': loss, 'batch_num': batch_num}
+    torch.save(checkpoint, path)
 
-
-disc = Discriminator(Num_ColorChannels, feature_d).to(device)
+cri = critic(Num_ColorChannels, feature_d).to(device)
 gen = Generator(Noise_Dim, Num_ColorChannels, feature_g).to(device)
 
-Initialize_Weight(disc)
+Initialize_Weight(critic)
 Initialize_Weight(gen)
 
-disc_Optim = torch.optim.Adam(disc.parameters(), lr=lr, betas=(0.5, 0.999))
-gen_Optim = torch.optim.Adam(gen.parameters(), lr=lr, betas=(0.5, 0.999))
-criterion = nn.BCELoss()
+critic_Optim = torch.optim.RMSprop(critic.parameters(), lr=lr)
+gen_Optim = torch.optim.RMSprop(gen.parameters(), lr=lr)
+
 
 if Model_Load is True:
-    ModelLocation_disc = TrainDataLoc + ModelVersion_disc
-    Loaded_Checkpoint_disc = torch.load(ModelLocation_disc)
-    disc.load_state_dict(Loaded_Checkpoint_disc['State_dict'])
-    disc_Optim.load_state_dict(Loaded_Checkpoint_disc['Optimizer'])
-    epoch_saved = Loaded_Checkpoint_disc['epoch']
-    batch_saved = Loaded_Checkpoint_disc['batch_num']
-    loss_disc = Loaded_Checkpoint_disc['loss']
+    ModelLocation_critic = TrainDataLoc + ModelVersion_critic
+    Loaded_Checkpoint_critic = torch.load(ModelLocation_critic)
+    critic.load_state_dict(Loaded_Checkpoint_critic['State_dict'])
+    critic_Optim.load_state_dict(Loaded_Checkpoint_critic['Optimizer'])
+    epoch_saved = Loaded_Checkpoint_critic['epoch']
+    batch_saved = Loaded_Checkpoint_critic['batch_num']
+    loss_critic = Loaded_Checkpoint_critic['loss']
 
     ModelLocation_gen = TrainDataLoc + ModelVersion_gen
     Loaded_Checkpoint_gen = torch.load(ModelLocation_gen)
     gen.load_state_dict(Loaded_Checkpoint_gen['State_dict'])
     gen_Optim.load_state_dict(Loaded_Checkpoint_gen['Optimizer'])
     loss_gen = Loaded_Checkpoint_gen['loss']
-
-# def test(N, in_channel, Height, Width, Noise_dim):
-#     InputDisc = torch.randn(N, in_channel, Height, Width)
-#
-#     disc = Discriminator(in_channel, feature_d=8)
-#     Initialize_Weight(disc)
-#     # assert disc(Input).shape == (N, 1, 1, 1)
-#     TestDisc = disc(InputDisc)
-#     print(F"Discriminator test: {TestDisc.shape}")
-#
-#     img_channel = in_channel
-#     InputGen_Noise = torch.randn(N, Noise_dim, 1, 1)
-#     gen = Generator(Noise_dim, img_channel, 8)
-#     Initialize_Weight(gen)
-#     # assert gen(InputGen_Noise).shape == (N, in_channel, 1, 1)
-#     TestGen = gen(InputGen_Noise)
-#     print(f"Generator test: {TestGen.shape}")
-
-# test(8, 3, 64, 64, 100)
-
-# sys.exit("Testing the Disc and Genrator networks")
 
 
 transforms = transforms.Compose(
@@ -124,42 +107,36 @@ for epoch in range(num_epochs):
         epoch = epoch_saved + epoch
         if batch_saved == len(IterationOfTheData):
             epoch = epoch + 1
-
     for Batch_Index, (Real_Image, _) in enumerate(IterationOfTheData):
         if Model_Load is True:
             Batch_Index = batch_saved + Batch_Index + 1
 
         Real_Image = Real_Image.to(device)
-        Noise_input = torch.randn(batch_size, Noise_Dim, 1, 1).to(device)
-        Gen_Noise = gen(Noise_input)
+        for i in range(Critic_Iteration):
+            Noise_input = torch.randn(batch_size, Noise_Dim, 1, 1).to(device)
+            Gen_Noise = gen(Noise_input)
+            critic_real = critic(Real_Image).reshape(-1)
+            critic_fake = critic(Gen_Noise).reshape(-1)
 
-        # Disc Loss
-        Disc_ReImg = disc(Real_Image).reshape(-1)
-        Disc_Gen_Noise = disc(Gen_Noise.detach()).reshape(-1)  # kind of similar to squeeze
+            loss_critic = -(torch.mean(critic_real) - torch.mean(critic_fake)) #Minimizing the negative format of this equation do that technically we arw maximizing it according to the paper!
+            critic.zero_grad()
+            loss_critic.backward(retain_graph=True)
+            critic_Optim.step()
 
-        loss_disc_real = criterion(Disc_ReImg, torch.ones_like(Disc_ReImg))
-        loss_disc_fake = criterion(Disc_Gen_Noise, torch.zeros_like(Disc_Gen_Noise))
-        loss_disc = (loss_disc_real + loss_disc_fake) / 2
-        disc.zero_grad()
-        loss_disc.backward() #Retain graph could be set to True instead of detach a few lines above here, since we are disc(gen_Fake_Image) and performing another backprop below that involves disc(gen_Fake_Image)
-        disc_Optim.step()
+            for p in critic.parameters():
+                p.data.clamp_(-Weight_Clip, +Weight_Clip)
+            
 
-        # Gen Loss
-        Disc_Gen_Noise = disc(Gen_Noise).reshape(-1)
-        loss_gen = criterion(Disc_Gen_Noise, torch.ones_like(Disc_Gen_Noise))
-        gen.zero_grad()
-        loss_gen.backward()
-        gen_Optim.step()
 
         if Batch_Index % 200 == 0 and Batch_Index != 0:
             if Model_Save == True:
-                ModelSave_func(disc, disc_Optim, loss_disc, batch_num=Batch_Index, epoch_num=epoch, path=f'{TrainDataLoc}/Model_Save/Discriminator_Epoch{epoch}_BatchIdx{Batch_Index}.pth')
+                ModelSave_func(critic, critic_Optim, loss_critic, batch_num=Batch_Index, epoch_num=epoch, path=f'{TrainDataLoc}/Model_Save/criticriminator_Epoch{epoch}_BatchIdx{Batch_Index}.pth')
                 ModelSave_func(gen, gen_Optim, loss_gen, batch_num=Batch_Index, epoch_num=epoch, path=f'{TrainDataLoc}/Model_Save/Generator_Epoch{epoch}_BatchIdx{Batch_Index}.pth')
 
         if Batch_Index % 10 == 0:
             print(f"Batch: [{Batch_Index}/{len(IterationOfTheData)}] \ "
                   f"eEpoch: [{epoch}/{num_epochs}] \ "
-                  f"Loss Discriminator: {loss_disc: 0.4f} & Loss Generator: {loss_gen: 0.4f}"
+                  f"Loss criticriminator: {loss_critic: 0.4f} & Loss Generator: {loss_gen: 0.4f}"
             )
 
             with torch.no_grad():
