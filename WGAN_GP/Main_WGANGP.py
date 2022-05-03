44dcf38a4f96c26c7f4214ed8a64df7d9b3eaabc
Javid <j.abderezaei@gmail.com>
2022-05-03 09:51:18 -0700

    Corrected the batchsize problem in the code

--- WGAN_GP/Main_WGANGP.py
+++ WGAN_GP/Main_WGANGP.py
@@ -12,11 +12,11 @@ import matplotlib.pyplot as plt
 import sys
 
 Model_Save = True
-Model_Load = False
+Model_Load = True
 
 if Model_Load is True:
-    ModelVersion_crit = "/Model_Save_WGAN/Critic_Epochxx_BatchIdxxx.pth"
-    ModelVersion_gen = "/Model_Save_WGAN/Generator_Epochxx_BatchIdxxx.pth"
+    ModelVersion_crit = "/Model_Save_WGANGP/Critic_Epoch0_BatchIdx1580.pth"
+    ModelVersion_gen = "/Model_Save_WGANGP/Generator_Epoch0_BatchIdx1580.pth"
 
 FFHQdataset = False
 MNISTdataset = False
@@ -133,12 +133,12 @@ for epoch in range(num_epochs):
         loss_gen.backward()
         gen_Optim.step()
 
-        if Batch_Index % 5 == 0 and Batch_Index != 0:
+        if Batch_Index % 800 == 0 and Batch_Index != 0:
             if Model_Save == True:
                 ModelSave_func(crit, crit_Optim, loss_crit, batch_num=Batch_Index, epoch_num=epoch, path=f'{TrainDataLoc}/Model_Save_WGANGP/Critic_Epoch{epoch}_BatchIdx{Batch_Index}.pth')
                 ModelSave_func(gen, gen_Optim, loss_gen, batch_num=Batch_Index, epoch_num=epoch, path=f'{TrainDataLoc}/Model_Save_WGANGP/Generator_Epoch{epoch}_BatchIdx{Batch_Index}.pth')
 
-        if Batch_Index % 10 == 0:
+        if Batch_Index % 50 == 0:
             print(f"Batch: [{Batch_Index}/{len(IterationOfTheData)}] \ "
                   f"Epoch: [{epoch}/{num_epochs}] \ "
                   f"Loss critic: {loss_crit: 0.4f} & Loss Generator: {loss_gen: 0.4f}"
