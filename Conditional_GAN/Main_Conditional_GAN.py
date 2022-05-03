992544b33123f00c34d8399afca5ab7880764ea1
Javid <j.abderezaei@gmail.com>
2022-05-03 10:24:21 -0700

    Starting the Conditional GAN project

---
+++ Conditional_GAN/Main_Conditional_GAN.py
@@ -0,0 +1,164 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+# from DataLoader import FFHQ_Dataset
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+from Model import critic, Generator, Initialize_Weight
+from Utils import gradient_penalty
+import matplotlib.pyplot as plt
+import sys
+
+Model_Save = True
+Model_Load = False
+
+if Model_Load is True:
+    ModelVersion_crit = "/Model_Save_WGANGP/Critic_Epoch0_BatchIdx2400.pth"
+    ModelVersion_gen = "/Model_Save_WGANGP/Generator_Epoch0_BatchIdx2400.pth"
+
+FFHQdataset = False
+MNISTdataset = False
+Celebdataset = True
+
+if FFHQdataset == True:
+    TrainDataLoc = '/Users/javidabderezaei/Downloads/TransferToServer/Explicit-GAN-Project/FFHQ/Images_Combined'
+if MNISTdataset == True:
+    TrainDataLoc = "/Users/javidabderezaei/Downloads/TransferToServer/Explicit-GAN-Project/MNIST_Data"
+if Celebdataset == True:
+    TrainDataLoc = "/Users/javidabderezaei/Downloads/TransferToServer/Explicit-GAN-Project/Celeb"
+
+## Hyperparameters
+lr = 1e-4
+device = "cuda" if torch.cuda.is_available() else "cpu"
+Noise_Dim = 128
+Image_Width = 64
+Image_Height = 64
+Num_ColorChannels = 3
+batch_size = 128
+num_epochs = 100
+feature_d = 64
+feature_g = 64
+Num_Imgs_On_Tensorboard = 32
+Critic_Iteration = 5
+Lambda_GradientPenalty = 10
+
+
+def ModelSave_func(model, optimization, loss, batch_num, epoch_num, path):
+    checkpoint = {'epoch': epoch_num, 'State_dict': model.state_dict(), 'Optimizer': optimization.state_dict(),
+                  'loss': loss, 'batch_num': batch_num}
+    torch.save(checkpoint, path)
+
+
+crit = critic(Num_ColorChannels, feature_d).to(device)
+gen = Generator(Noise_Dim, Num_ColorChannels, feature_g).to(device)
+
+Initialize_Weight(crit)
+Initialize_Weight(gen)
+
+crit_Optim = torch.optim.Adam(crit.parameters(), lr=lr, betas=(0.0, 0.9))
+gen_Optim = torch.optim.Adam(gen.parameters(), lr=lr, betas=(0.0, 0.9))
+
+if Model_Load is True:
+    ModelLocation_crit = TrainDataLoc + ModelVersion_crit
+    Loaded_Checkpoint_crit = torch.load(ModelLocation_crit)
+    crit.load_state_dict(Loaded_Checkpoint_crit['State_dict'])
+    crit_Optim.load_state_dict(Loaded_Checkpoint_crit['Optimizer'])
+    epoch_saved = Loaded_Checkpoint_crit['epoch']
+    batch_saved = Loaded_Checkpoint_crit['batch_num']
+    loss_crit = Loaded_Checkpoint_crit['loss']
+
+    ModelLocation_gen = TrainDataLoc + ModelVersion_gen
+    Loaded_Checkpoint_gen = torch.load(ModelLocation_gen)
+    gen.load_state_dict(Loaded_Checkpoint_gen['State_dict'])
+    gen_Optim.load_state_dict(Loaded_Checkpoint_gen['Optimizer'])
+    loss_gen = Loaded_Checkpoint_gen['loss']
+
+transforms = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize([Image_Width, Image_Height]),
+        transforms.Normalize(
+            [0.5 for ii in range(Num_ColorChannels)], [0.5 for ii in range(Num_ColorChannels)]
+        )
+    ]
+)
+
+if FFHQdataset == True:
+    DataLoading = FFHQ_Dataset(TrainDataLoc, transform=transforms)
+    IterationOfTheData = DataLoader(DataLoading, batch_size=batch_size, shuffle=True)
+
+if MNISTdataset == True:
+    DataLoading = datasets.MNIST(root=TrainDataLoc, transform=transforms, download=True)
+    IterationOfTheData = DataLoader(DataLoading, batch_size=batch_size, shuffle=True)
+
+if Celebdataset == True:
+    TrainDataLocFinal = TrainDataLoc + "/Celeb_Dataset"
+    DataLoading = datasets.ImageFolder(root=TrainDataLocFinal, transform=transforms)
+    IterationOfTheData = DataLoader(DataLoading, batch_size=batch_size, shuffle=True)
+
+fixed_noise_For_Tensorboard = torch.randn(Num_Imgs_On_Tensorboard, Noise_Dim, 1, 1).to(device)
+writer_fake = SummaryWriter(f"{TrainDataLoc}/Tensorboard/runs/Fake")
+writer_real = SummaryWriter(f"{TrainDataLoc}/Tensorboard/runs/Real")
+step = 0
+
+for epoch in range(num_epochs):
+    if Model_Load is True:
+        epoch = epoch_saved + epoch
+        if batch_saved == len(IterationOfTheData):
+            epoch = epoch + 1
+    for Batch_Index, (Real_Image, _) in enumerate(IterationOfTheData):
+        if Model_Load is True:
+            Batch_Index = batch_saved + Batch_Index + 1
+
+        batch_size = Real_Image.shape[0]
+        Real_Image = Real_Image.to(device)
+        for i in range(Critic_Iteration):
+            Noise_input = torch.randn(batch_size, Noise_Dim, 1, 1).to(device)
+
+            ## Discriminator Loss
+            Gen_Img = gen(Noise_input)
+            crit_real = crit(Real_Image).reshape(-1)
+            crit_fake = crit(Gen_Img).reshape(-1)
+            gp = gradient_penalty(Gen_Img, Real_Image, crit, device)
+            loss_crit = -(torch.mean(crit_real) - torch.mean(
+                crit_fake)) + Lambda_GradientPenalty * gp  # Minimizing the negative format of this equation do that technically we arw maximizing it according to the paper!
+            crit.zero_grad()
+            loss_crit.backward(retain_graph=True)
+            crit_Optim.step()
+
+            ## Generator Loss: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
+        loss_gen = -torch.mean(crit(Gen_Img).reshape(-1))
+        gen.zero_grad()
+        loss_gen.backward()
+        gen_Optim.step()
+
+        if Batch_Index % 800 == 0 and Batch_Index != 0:
+            if Model_Save == True:
+                ModelSave_func(crit, crit_Optim, loss_crit, batch_num=Batch_Index, epoch_num=epoch,
+                               path=f'{TrainDataLoc}/Model_Save_WGANGP/Critic_Epoch{epoch}_BatchIdx{Batch_Index}.pth')
+                ModelSave_func(gen, gen_Optim, loss_gen, batch_num=Batch_Index, epoch_num=epoch,
+                               path=f'{TrainDataLoc}/Model_Save_WGANGP/Generator_Epoch{epoch}_BatchIdx{Batch_Index}.pth')
+
+        if Batch_Index % 5 == 0:
+            print(f"Batch: [{Batch_Index}/{len(IterationOfTheData)}] \ "
+                  f"Epoch: [{epoch}/{num_epochs}] \ "
+                  f"Loss critic: {loss_crit: 0.4f} & Loss Generator: {loss_gen: 0.4f}"
+                  )
+
+            with torch.no_grad():
+                fake_Image = gen(fixed_noise_For_Tensorboard)
+
+                img_grid_fake = torchvision.utils.make_grid(fake_Image, normalize=True)
+                img_grid_real = torchvision.utils.make_grid(Real_Image[:Num_Imgs_On_Tensorboard], normalize=True)
+
+                writer_fake.add_image(
+                    "Fake Images", img_grid_fake, global_step=step
+                )
+
+                writer_real.add_image(
+                    "Real Images", img_grid_real, global_step=step
+                )
+
+                step += 1
---
+++ Conditional_GAN/Model.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+
+
+class critic(nn.Module):
+    def __init__(self, in_channels, feature_d):
+        super(critic, self).__init__() #Differentce between super(Discriminator) and super() should be checked!
+
+        self.disc = nn.Sequential(
+            nn.Conv2d(in_channels, feature_d, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            self.ConvBlock(feature_d, feature_d*2, kernel_size=4, stride=2, padding=1),
+            self.ConvBlock(feature_d*2, feature_d*4, kernel_size=4, stride=2, padding=1),
+            self.ConvBlock(feature_d*4, feature_d*8, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(feature_d*8, 1, kernel_size=4, stride=2, padding=0),
+        )
+
+    def ConvBlock(self, in_channels, out_channel, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channel, kernel_size, stride, padding, bias=False),
+            nn.InstanceNorm2d(out_channel, affine=True),
+            nn.LeakyReLU(0.2)
+        )
+    def forward(self, x):
+        return self.disc(x)
+
+class Generator(nn.Module):
+    def __init__(self, Noise_Dim, in_channels_img, feature_g):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            self.ConvTransposeBlock(Noise_Dim, feature_g*16, kernel_size=4, stride=1, padding=0),
+            self.ConvTransposeBlock(feature_g*16, feature_g*8, kernel_size=4, stride=2, padding=1),
+            self.ConvTransposeBlock(feature_g*8, feature_g*4, kernel_size=4, stride=2, padding=1),
+            self.ConvTransposeBlock(feature_g*4, feature_g*2, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(feature_g*2, in_channels_img, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
+
+    def ConvTransposeBlock(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+    def forward(self, x):
+        return self.gen(x)
+
+def Initialize_Weight(Model):
+    for m in Model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d, nn.InstanceNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+
+# def test(N, in_channel, Height, Width, Noise_dim):
+#     InputDisc = torch.randn(N, in_channel, Height, Width)
+#
+#     disc = Discriminator(in_channel, feature_d=8)
+#     Initialize_Weight(disc)
+#     # assert disc(Input).shape == (N, 1, 1, 1)
+#     TestDisc = disc(InputDisc)
+#     print(F"Discriminator test: {TestDisc.shape}")
+#
+#     img_channel = in_channel
+#     InputGen_Noise = torch.randn(N, Noise_dim, 1, 1)
+#     gen = Generator(Noise_dim, img_channel, 8)
+#     Initialize_Weight(gen)
+#     # assert gen(InputGen_Noise).shape == (N, in_channel, 1, 1)
+#     TestGen = gen(InputGen_Noise)
+#     print(f"Generator test: {TestGen.shape}")
+
+# test(8, 3, 64, 64, 100)
+
+# sys.exit("Testing the Disc and Genrator networks")
+
+# if __name__== "__Main__":
+#     Model
