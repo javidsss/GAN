5b1d1acdf31eefee168db6c4588564e26ae6eafb
Javid <j.abderezaei@gmail.com>
2022-05-20 16:54:02 -0700

    Working organizer for Cerebellum

--- WGAN_GP/DataOrganizer.py
+++ WGAN_GP/DataOrganizer.py
@@ -16,83 +16,9 @@ import torchvision.transforms as transforms
 import nibabel as nib
 # import nibabel.freesurfer.mghformat as mgh
 
-# class CerebellumData(Dataset):
-#     def __init__(self, train_loc, scale_factor=None, transform=None):
-#         super(CerebellumData, self).__init__()
-#
-#         self.train_loc = train_loc
-#         self.train_foldernames = os.listdir(self.train_loc)
-#         self.train_foldernames_NoExtra = []
-#         for FolderNames in self.train_foldernames:
-#             if FolderNames.startswith('Subj'):
-#                 self.train_foldernames_NoExtra.append(FolderNames)
-#
-#         self.train_mask_foldernames = os.listdir(self.train_loc)
-#         self.train_mask_foldernames_NoExtra = []
-#         for FolderNames in self.train_mask_foldernames:
-#             if FolderNames.startswith('Subj'):
-#                 self.train_mask_foldernames_NoExtra.append(FolderNames)
-#
-#         self.scale_factor = scale_factor
-#         self.transform = transform
-#
-#     def __getitem__(self, index):
-#         train_loc_final = os.path.join(self.train_loc, self.train_foldernames_NoExtra[index], 'T1_LPI.nii')
-#         train_data_numpy3D = np.array(nib.load(train_loc_final).get_fdata())
-#         SliceToSave = math.ceil(train_data_numpy3D.shape[0] / 2)
-#         train_data_numpy = np.squeeze(train_data_numpy3D[SliceToSave, :, : ])
-#         [h, w] = train_data_numpy.shape
-#         if self.scale_factor is not None:
-#             train_data_final = np.zeros([int(math.ceil(h / self.scale_factor)), int(math.ceil(w / self.scale_factor))], dtype='float32')
-#             train_data_final[:, :] = cv2.resize(train_data_numpy[:, :], dsize=(int(math.ceil(w / self.scale_factor)), int(h / self.scale_factor)))  # open cvs size input automatically transposes the sizess!!!
-#         else:
-#             train_data_final = train_data_numpy
-#
-#
-#         train_mask_loc_final = os.path.join(self.train_loc, self.train_mask_foldernames_NoExtra[index], 'aseg_LPI.nii')
-#         train_mask_numpy3D = np.array(nib.load(train_mask_loc_final).get_fdata())
-#         train_mask_numpy = np.squeeze(train_mask_numpy3D[SliceToSave, :, :])
-#         if self.scale_factor is not None:
-#             train_mask_final = np.zeros([int(math.ceil(h / self.scale_factor)), int(math.ceil(w / self.scale_factor))], dtype='float32')
-#             train_mask_final[:, :] = cv2.resize(train_mask_numpy[:, :], dsize=(int(math.ceil(w / self.scale_factor)), int(h / self.scale_factor))) #open cvs size input automatically transposes the sizess!!!
-#         else:
-#             train_mask_final = train_mask_numpy
-#
-#
-#         if self.transform is not None:
-#             train_mask_finalIndex = np.isin(train_mask_final, [6, 7, 8, 45, 46, 47])
-#             train_mask_final_Ones = train_mask_finalIndex.astype(int)
-#             MaskedImage = train_mask_final_Ones*train_data_final
-#             MaskedImage = self.transform(MaskedImage)
-#             # nib.save(maskedimage.to(torch.float32), os.path.join(train_loc_final, '2DTonsil.nii'))
-#
-#         else:
-#
-#             train_mask_finalIndex = np.isin(train_mask_final, [6, 7, 8, 45, 46, 47])
-#             train_mask_final_Ones = train_mask_finalIndex.astype(int)
-#             MaskedImage = train_mask_final_Ones*train_data_final
-#             Rotated = np.rot90(train_data_final, 3, [0, 1])
-#             plt.imshow(train_data_final)  # T1-LPI
-#             plt.figure()
-#             plt.imshow(Rotated)  # Rotated T1-LPI
-#             plt.figure()
-#             plt.imshow(train_mask_final)  # T1
-#             plt.figure()
-#             plt.imshow(Rotated-train_mask_final)
-#
-#             SavePath = os.path.join(self.train_loc, self.train_foldernames_NoExtra[index], 'MidSlice_Tonsil')
-#             print(SavePath)
-#             # nib.save(nib.Nifti1Image(maskedimage, affine=np.eye(4)), SavePath)
-#
-#         return 0, 0
-#
-#     def __len__(self):
-#         return len(self.train_mask_foldernames_NoExtra)
-
-
-class CerebellumData_LPI_To_Normal(Dataset):
+class CerebellumData(Dataset):
     def __init__(self, train_loc, scale_factor=None, transform=None):
-        super(CerebellumData_LPI_To_Normal, self).__init__()
+        super(CerebellumData, self).__init__()
 
         self.train_loc = train_loc
         self.train_foldernames = os.listdir(self.train_loc)
@@ -111,74 +37,56 @@ class CerebellumData_LPI_To_Normal(Dataset):
         self.transform = transform
 
     def __getitem__(self, index):
-        train_loc_final = os.path.join(self.train_loc, self.train_foldernames_NoExtra[index], 'T1_LPI_Test.nii')
+
+
+        FileNamesInFolder = os.listdir(os.path.join(self.train_loc, self.train_foldernames_NoExtra[index]))
+
+        for FileNames in FileNamesInFolder:
+            if FileNames.startswith('T1_LPI') and not FileNames.endswith('seg1.nii') and not FileNames.endswith('seg2.nii') and FileNames.endswith('.nii'):
+                LastName_T1 = FileNames
+            if FileNames.startswith('aseg_LPI'):
+                LastName_aseg = FileNames
+
+        train_loc_final = os.path.join(self.train_loc, self.train_foldernames_NoExtra[index], LastName_T1)
         train_data_numpy3D = np.array(nib.load(train_loc_final).get_fdata())
         SliceToSave = math.ceil(train_data_numpy3D.shape[0] / 2)
-        # train_data_numpy3D_Rotated = np.rot90(train_data_numpy3D, 1, [0, 1])
-        # train_data_numpy3D_Rotated_Permuted = np.transpose(train_data_numpy3D_Rotated, [0, 2, 1])
-        # train_data_numpy3D_Rotated_Permuted_Fliped = np.flipud(train_data_numpy3D_Rotated_Permuted)
-
-        # train_data_numpy_AllSlices = np.zeros(np.shape(train_data_numpy3D))
-
-        train_data_numpy3D_Permuted = np.transpose(train_data_numpy3D, [0, 2, 1])
-
-        # for Slicess in range(train_data_numpy_AllSlices.shape[1]):
-        #     EachSlice = np.squeeze(train_data_numpy3D_Permuted[:, Slicess, :])
-        #     train_data_numpy_AllSlices[:, Slicess, :] = np.flipud(np.rot90(EachSlice, 1, [0, 1]))
-
-
-        train_data_numpy = np.squeeze(train_data_numpy3D_Permuted[:, :, SliceToSave+20])
-
-
+        train_data_numpy = np.squeeze(train_data_numpy3D[SliceToSave, :, : ])
         [h, w] = train_data_numpy.shape
         if self.scale_factor is not None:
-            train_data_final = np.zeros([int(math.ceil(h / self.scale_factor)), int(math.ceil(w / self.scale_factor))],
-                                        dtype='float32')
-            train_data_final[:, :] = cv2.resize(train_data_numpy[:, :], dsize=(int(math.ceil(w / self.scale_factor)),
-                                                                               int(h / self.scale_factor)))  # open cvs size input automatically transposes the sizess!!!
+            train_data_final = np.zeros([int(math.ceil(h / self.scale_factor)), int(math.ceil(w / self.scale_factor))], dtype='float32')
+            train_data_final[:, :] = cv2.resize(train_data_numpy[:, :], dsize=(int(math.ceil(w / self.scale_factor)), int(h / self.scale_factor)))  # open cvs size input automatically transposes the sizess!!!
         else:
             train_data_final = train_data_numpy
 
-        train_mask_loc_final = os.path.join(self.train_loc, self.train_mask_foldernames_NoExtra[index], 'T1.nii')
+        train_mask_loc_final = os.path.join(self.train_loc, self.train_mask_foldernames_NoExtra[index], LastName_aseg)
         train_mask_numpy3D = np.array(nib.load(train_mask_loc_final).get_fdata())
-
-
-        train_mask_numpy = np.fliplr(np.squeeze(train_mask_numpy3D[:, :, SliceToSave+20]))
-
-
+        train_mask_numpy = np.squeeze(train_mask_numpy3D[SliceToSave, :, :])
         if self.scale_factor is not None:
-            train_mask_final = np.zeros([int(math.ceil(h / self.scale_factor)), int(math.ceil(w / self.scale_factor))],
-                                        dtype='float32')
-            train_mask_final[:, :] = cv2.resize(train_mask_numpy[:, :], dsize=(int(math.ceil(w / self.scale_factor)),
-                                                                               int(h / self.scale_factor)))  # open cvs size input automatically transposes the sizess!!!
+            train_mask_final = np.zeros([int(math.ceil(h / self.scale_factor)), int(math.ceil(w / self.scale_factor))], dtype='float32')
+            train_mask_final[:, :] = cv2.resize(train_mask_numpy[:, :], dsize=(int(math.ceil(w / self.scale_factor)), int(h / self.scale_factor))) #open cvs size input automatically transposes the sizess!!!
         else:
             train_mask_final = train_mask_numpy
+
 
         if self.transform is not None:
             train_mask_finalIndex = np.isin(train_mask_final, [6, 7, 8, 45, 46, 47])
             train_mask_final_Ones = train_mask_finalIndex.astype(int)
             MaskedImage = train_mask_final_Ones * train_data_final
             MaskedImage = self.transform(MaskedImage)
-            # nib.save(maskedimage.to(torch.float32), os.path.join(train_loc_final, '2DTonsil.nii'))
+            nib.save(MaskedImage.to(torch.float32), os.path.join(train_loc_final, 'MidSlice_Cerebellum_LPI.nii'))
 
         else:
 
             train_mask_finalIndex = np.isin(train_mask_final, [6, 7, 8, 45, 46, 47])
             train_mask_final_Ones = train_mask_finalIndex.astype(int)
             MaskedImage = train_mask_final_Ones * train_data_final
-            # Rotated = np.rot90(train_data_final, 3, [0, 1])
-
-            plt.imshow(train_data_final)  # T1-LPI
-            plt.figure()
-            plt.imshow(train_mask_final)  # T1
-            plt.figure()
-            plt.imshow(train_data_final - train_mask_final)
-
-            SavePath = os.path.join(self.train_loc, self.train_foldernames_NoExtra[index], 'T1_Converted_From_LPI')
+            SavePath = os.path.join(self.train_loc, self.train_foldernames_NoExtra[index], 'MidSlice_Cerebellum_LPI')
             print(SavePath)
-            # nib.save(nib.Nifti1Image(maskedimage, affine=np.eye(4)), SavePath)
+            nib.save(nib.Nifti1Image(MaskedImage, affine=np.eye(4)), SavePath)
 
-        return 0, 0
+        print(train_loc_final)
+        # return train_data_final, train_mask_final_Ones, MaskedImage
+        return 0, 0, MaskedImage
 
     def __len__(self):
         return len(self.train_mask_foldernames_NoExtra)
@@ -198,7 +106,7 @@ Noise_Dim = 128
 Image_Width = 64
 Image_Height = 64
 Num_ColorChannels = 1
-batch_size = 8
+batch_size = 1
 num_epochs = 100
 feature_d = 64
 feature_g = 64
@@ -217,9 +125,11 @@ transforms = transforms.Compose(
 )
 
 
-# DataLoadPractice = CerebellumData(TrainDataLoc, transform=None)
-DataLoadPractice = CerebellumData_LPI_To_Normal(TrainDataLoc, transform=None)
+DataLoadPractice = CerebellumData(TrainDataLoc, transform=None)
 IterationOfTheData = DataLoader(DataLoadPractice, batch_size=batch_size, shuffle=False)
 
-for i, image in enumerate(IterationOfTheData):
-    plt.imshow(image[0,0,:,:,])
+for i, (train_data_final, train_mask_final_Ones, MaskedImage) in enumerate(IterationOfTheData):
+
+    plt.imshow(MaskedImage[0,:,:])
+    plt.close()
+    print(f'Save_Index_{i}')
